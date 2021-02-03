b9a00c4e7824dadbfd1f2b634ea9c234b7e968a3
Arshad Ali <arshadalitalwar@gmail.com>
2021-02-03 14:17:17 +0530

    Dashboar and navbar done

--- couchsurfer-app/src/App.js
+++ couchsurfer-app/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Routes from './Routes/Routes';
-import './App.css';
+import "./App.css";
+import Routes from "./Routes/Routes";
+import "./App.css";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Navbar from "./Components/Navbar/Navbar";
 export default function App() {
   return (
     <div className="App">
-      <Routes/>
+      <Routes />
     </div>
   );
 }
