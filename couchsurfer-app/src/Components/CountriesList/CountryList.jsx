687dbc3f43f598323ee25b31c33d08ebc7e39bd3
Gayathry <gayathryjm@gmail.com>
2021-02-03 18:30:54 +0530

    modified

--- couchsurfer-app/src/Components/CountriesList/CountryList.jsx
+++ couchsurfer-app/src/Components/CountriesList/CountryList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import styles from "./CountryList.module.css";
+import styles from "./CountryList.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getCountry } from "../../Redux/Country/action";
 import { useHistory } from "react-router-dom";
@@ -11,7 +11,7 @@ function CountryLists() {
   console.log(countries,country)
   const history = useHistory();
   const dispatch = useDispatch();
-  
+
   React.useEffect(() => {  
     dispatch(
       getCountry()
@@ -20,12 +20,15 @@ function CountryLists() {
 
   return (
     <div>
-      <div>
-        <input type="text" placeholder="Search for something..." />
-        <button>Search</button>
-      </div>
-      
-      {
+      {/* <div style={{background : "url(https://farm1.staticflickr.com/145/356953720_e2b12c64ea_b.jpg) no-repeat center 50%", 
+           backgroundSize: "cover",
+          height:"40vh", 
+          padding:"0px",
+          color:"white"}}>
+       
+       <h1>India</h1>
+        <br/>
+        {
         countries?.map((country_item)=>(
           country === country_item.name ?  
             country_item.cities.map(city=>
@@ -34,15 +37,16 @@ function CountryLists() {
           : null
         ))
       }
-
-      {
-        
-          country === "India" ?   <CountryIndia/> : null
-      }
-    
-      {/* <div>
-        <CountryIndia/>
       </div> */}
+      
+
+      {/* {
+        
+          country === "India" ?   <CountryIndia countryName={country} cityList={countries} /> : null
+      } */}
+      
+      <CountryIndia countryName={country} cityList={countries} />
+    
     </div>
   );
 }
